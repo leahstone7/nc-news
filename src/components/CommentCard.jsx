44f0e4960a85541cf8e28e8f79b4dc8b6642d3a5
Leah Stone <leahstone@MacBook-Pro-2.local>
2025-05-22 11:03:47 +0100

    tried to use base-url but import wouldn't work, used day.js to improve date format on comments

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,11 +1,13 @@
 import Card from 'react-bootstrap/Card'
+import dayjs from 'dayjs'
 
 function CommentCard({commentAuthor, commentBody, commentCreatedAt, commentVotes, article_id}){
+const newDate = dayjs(commentCreatedAt).format('MMM D, YYYY h:mm A')
 
 return (
     <Card className='comment-card'>
     <Card.Text>{commentAuthor}: {commentBody}</Card.Text>
-    <Card.Text>Posted: {commentCreatedAt}</Card.Text>
+    <Card.Text>Posted: {newDate}</Card.Text>
     <Card.Text>Votes: {commentVotes}</Card.Text>
     </Card>
 )
