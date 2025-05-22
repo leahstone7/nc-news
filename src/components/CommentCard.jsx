f51d73c5822a9fe90f180a82ecdde7cf11981f56
Leah Stone <leahstone@MacBook-Pro-2.local>
2025-05-22 13:58:48 +0100

    task 9 complete delete own comments

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,14 +1,21 @@
 import Card from 'react-bootstrap/Card'
 import dayjs from 'dayjs'
 
-function CommentCard({commentAuthor, commentBody, commentCreatedAt, commentVotes, article_id}){
+function CommentCard({commentAuthor, commentBody, commentCreatedAt, commentVotes, article_id, user, handleDelete, comment_id}){
 const newDate = dayjs(commentCreatedAt).format('MMM D, YYYY h:mm A')
+
+
 
 return (
     <Card className='comment-card'>
     <Card.Text>{commentAuthor}: {commentBody}</Card.Text>
     <Card.Text>Posted: {newDate}</Card.Text>
     <Card.Text>Votes: {commentVotes}</Card.Text>
+    {commentAuthor === user && (
+        <div className='delete-comment-container'>
+            <button className="delete-comment-button" onClick={() => handleDelete(comment_id)}>Delete comment</button>
+        </div>
+    )}
     </Card>
 )
 }
